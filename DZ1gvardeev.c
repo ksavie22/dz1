84a79e4ede0c43ace9128e4776ced99b3d5902ce
гвардеев <gvardeevstas@gmail.com>
2025-09-16 12:14:50 +0300

    Add files via upload

---
+++ DZ1gvardeev.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h> 
+void main()
+{
+	setlocale(LC_CTYPE, "RUS");
+
+
+	puts("****************************************************");
+	puts("*            __                                    *");
+	puts("*           |  |                                   *");
+	puts("*         __    __                                 *");
+	puts("*        |        |                                *");
+	puts("*         --    --                                 *");
+	puts("*        /  |  |                                   *");
+	puts("*       |    --                                    *");
+	puts("*       |                                          *");
+	puts("*       |                                          *");
+	puts("*       |                                          *");
+	puts("*                                                  *");
+	puts("*                                                  *");
+	puts("*                                                  *");
+	puts("****************************************************");
+
+
+
+}
